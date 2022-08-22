43fe7303fc487a215a55ca0a9ce3427018f3e164
mayakovsky <mayakovsky03@gmail.com>
2022-08-22 10:23:55 +0300

    Add component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // const myFirstSubscriber  = (event) => {
+    //     console.log('hello')
+    // }
+    //
+    // const mySecondSubscriber = (event) => {
+    //     console.log('give me your money')
+    // }
+
+    // const onClickHandler = (name: string) => {
+    //     console.log(name)
+    // }
+
+    // const foo1 = () => {
+    //     console.log(100200)
+    // }
+    // const foo2 = (number: number) => {
+    // console.log(number)
+    // }
+
+    const Button1Foo = (subscriber: string, age:number, address: string) => {
+    console.log(subscriber, age, address)
+    }
+    const Button2Foo = (subscriber: string) => {
+    console.log(subscriber)
+    }
+    const Button3Foo = () => {
+    console.log('im stupid button')
+    }
+
+
+    return (
+        <div className={'App'}>
+            {/*<button onClick ={(event) => onClickHandler('Vasya')}>myyoutubechannel</button>*/}
+            {/*<button onClick ={(event) => onClickHandler('VaNya')}>myyoutubechannel-2</button>*/}
+            {/*  <button onClick={foo1}>1</button>*/}
+            {/*  <button onClick={(event) => foo2(100200)}>2</button>*/}
+
+            <Button name={'myYoutubeChannel-1'} callBack={()=>Button1Foo('im vassya', 21, 'Live in Portugal')}/>
+            <Button name={'myYoutubeChannel-2'} callBack={()=>Button2Foo('im vannnya')}/>
+            <Button name={'myYoutubeChannel-3'} callBack={Button3Foo}/>
+        </div>
+    )
 }
 
 export default App;
